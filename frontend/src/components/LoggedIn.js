b246159fa5bf5ceb2f467d18fade7d6d7ac06243
Johan Blomberg <JohanBlomberg89@gmail.com>
2022-03-16 21:31:19 +0100

    Logout button

--- frontend/src/components/LoggedIn.js
+++ frontend/src/components/LoggedIn.js
@@ -4,6 +4,7 @@ function LoggedIn () {
       return (       
         <div className='form-inner'>
         <h1>Bra jobbat!</h1>
+        <button className="link"><Link to="/"> Logga Ut </Link></button>
         </div>
     )
 }
