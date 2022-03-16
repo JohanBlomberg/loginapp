f6514dff750bec7116d8fc8895031c1544289e69
Johan Blomberg <JohanBlomberg89@gmail.com>
2022-03-16 21:38:40 +0100

    Logout button

--- frontend/src/components/LoggedIn.js
+++ frontend/src/components/LoggedIn.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function LoggedIn () {
       return (       
-        <div className='form-inner'>
+        <div>
         <h1>Bra jobbat!</h1>
         <button className="link"><Link to="/"> Logga Ut </Link></button>
         </div>
